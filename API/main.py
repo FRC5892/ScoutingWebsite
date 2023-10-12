64635384134986d5d57f89bb448533b9d562f43d
Emiliano Lane <artyfoul@outlook.com>
2023-10-12 17:07:53 -0500

    Update

--- API/main.py
+++ API/main.py
@@ -10,18 +10,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/robot/{team_num}")
 def read_item(team_num: int):
-    final = '{ "Matches": [] }'
+    final = '{ "Matches": [], "statBotYear": [], "statBotEvent": [] }'
     final = json.loads(final)
-    statBotics = requests.get(f"https://api.statbotics.io/v2/team/{team_num}").json
-    gsheet = requests.get(f'https://xyz.com?team={team_num}')
-    final.append(statBotics)
-    for x in gsheet['Logs']:
-        temp = {
-            "Match": x['match'],
-            "Outcome": x['outcome'],
-            "Score": x['score']
-        }
-    return {"item_id": item_id}
+    statBoticsYear = requests.get(f"https://api.statbotics.io/v2/team_year/{team_num}/2024").json
+    statBoticsEvent = requests.get(f"https://api.statbotics.io/v2/team_event/{team_num}/2023txhou")
+    # gsheet = requests.get(f'https://xyz.com?team={team_num}')
+    final['statBotYear'].update(statBoticsYear)
+    final['statBotEvent'].update(statBoticsEvent)
+    # for x in gsheet['Logs']:
+    #     temp = {
+    #         "Match": x['match'],
+    #         "Outcome": x['outcome'],
+    #         "Score": x['score']
+    #     }
+    #     final['Matches'].update(temp)
+    return final
