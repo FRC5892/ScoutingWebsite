0fdd542359a3576107678dd0b494989259638a7a
Zagreus <61201959+Zagreus9723@users.noreply.github.com>
2023-11-27 15:42:48 -0600

    Update main.py
    Edited CORs technology.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import time
 import requests
 from fastapi import FastAPI
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
