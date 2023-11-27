bf2d813daf1d9b94d8b24189dc5672f464b4527f
Zagreus <61201959+Zagreus9723@users.noreply.github.com>
2023-11-27 15:46:44 -0600

    Update main.py
    Fix Cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
